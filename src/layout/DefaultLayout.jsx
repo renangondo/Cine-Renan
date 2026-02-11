09eff243fb3acb6005ac9d89c8ac94da3ffa1fd2
renangondo <renanluizgondo@gmail.com>
2026-02-11 20:14:22 -0300

    Feito as Rotas

---
+++ src/layout/DefaultLayout.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Header from '../components/Header'
+
+function DefaultLayout() {
+
+    return(
+        <>
+            <Header/>
+            <Outlet/>
+        </>
+
+
+    )
+
+}
+
+export  default DefaultLayout
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,14 +2,18 @@ import {Route, Routes} from 'react-router-dom'
 import Home from '../containers/Home'
 import Movies from '../containers/Movies'
 import Series from '../containers/Series' 
+import DefaultLayout from '../layout/DefaultLayout'
 
 
 function Router(){
     return(
         <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/filmes' element={<Movies/>} />
-            <Route path='/series' element={<Series/>} />
+            <Route element={<DefaultLayout/>}>
+                <Route path='/' element={<Home/>} />
+                <Route path='/filmes' element={<Movies/>} />
+                <Route path='/series' element={<Series/>} />
+            </Route>
+
         </Routes>
     )
 }
