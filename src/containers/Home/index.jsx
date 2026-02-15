2154e596507cf15c1454de73fd6d13cf89de0f95
renangondo <renanluizgondo@gmail.com>
2026-02-15 03:23:55 -0300

    Feito a tela Home

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,27 +1,46 @@
+import Button from '../../components/Button'
 import api from '../../services/api'
-import { Background } from './styles'
-import {useState} from 'react'
+import { Background, Poster, Info, Container, ContainerButton} from './styles'
+import { useState, useEffect } from 'react'
 
-function Home(){
+function Home() {
 
     const [movie, setMovie] = useState()
 
-    async function getMovies(){
-        const data = await api.get('/movie/popular')
-        setMovie(data.data.results[1])
-        console.log(data)
-    }
+    useEffect(() => {
 
-    getMovies()
+        async function getMovies() {
+            const { data: { results } } = await api.get('/movie/popular')
+            setMovie(results[1])
+
+        }
+        console.log(movie)
+        getMovies()
+
+    }, [])
+
+
+
     return (
 
         <>
-        {movie && ( 
-        <Background img="https://image.tmdb.org/t/p/original/1E5baAaEse26fej7uHcjOgEE2t2.jpg">
-            <h1>Home</h1>
-            <p>Essa é a Home</p>
-        </Background>
-        )}
+            {movie && (
+                <Background img={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}>
+                    <Container>
+                        <Info>
+                            <h1>{movie.title}</h1>
+                            <p>{movie.overview}</p>
+                            <ContainerButton>
+                                <Button red = {true}>Assista Agora</Button>
+                                <Button red = {false}>Assista o Trailer</Button>
+                            </ContainerButton>
+                        </Info>
+                        <Poster>
+                            <img alt='capa-do-filme' src={`https://image.tmdb.org/t/p/original${movie.poster_path}}`} />
+                        </Poster>
+                    </Container>
+                </Background>
+            )}
         </>
     )
 }
